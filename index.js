fa3d19376f88d23c9696eed57e9124a7c455fd11
Nemkev <ostegoelectric@gmail.com>
2020-06-20 22:23:50 +0300

    feat: resolve headers

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ mongoose.connect(
 const app = express();
 app.use(bodyParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(
   cors({
     credentials: true,
